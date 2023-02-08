440b6adf5df6fd79ebdee3e48d7397bb23bea587
Alexander Whipp <Whipp_Alexander@bah.com>
2023-02-08 14:09:59 -0500

    refactor: :hammer: Working 20-Day Historical Volume Queries

--- prod.sql
+++ prod.sql
@@ -2,40 +2,45 @@
 
 -- PRAGMA for faster queries
 pragma temp_store = memory;
-pragma mmap_size = 30000000000;
-pragma page_size = 32768;
+pragma mmap_size = 1099511627776;
+pragma page_size = 65536;
+.open data.db
 pragma optimize;
 .headers ON
 .timer ON
-.open data.db
 .mode csv
-.output volume20day.csv
+.output output.csv
+-- .mode stdout
 
--- Get the latest order for each order_id
+-- Step 1: Get the latest order for each order_id and sum the volume
 SELECT 
-    region_id, type_id, order_id, max(date), volume
+    region_id, type_id, order_id, MAX(date) as date, SUM(volume) as volume -- Get most recent order for order_id
 FROM orders
-GROUP BY region_id, type_id, order_id
-LIMIT 10;
+WHERE date >= date('now','-20 day') -- Number of days to average
+GROUP BY region_id, type_id, order_id -- Include order_id in order to average
 
--- Get the latest order for each order_id and sum the volume
-SELECT agg.region_id, agg.type_id, sum(agg.volume)
+-- Step 2: Collapse orders to date
+SELECT sum_agg.region_id, sum_agg.type_id, sum_agg.date, SUM(sum_agg.volume) as volume
 FROM (
     SELECT 
-        region_id, type_id, order_id, max(date), volume
-    FROM orders
-    GROUP BY region_id, type_id, order_id 
-    LIMIT 10
-) agg
-GROUP BY agg.region_id, agg.type_id
-
--- Get the latest order for each order_id and sum the volume
-SELECT agg.region_id, agg.type_id, AVG(agg.volume) -- Get average over N-days
-FROM (
-    SELECT 
-        region_id, type_id, order_id, MAX(date), volume -- Get most recent order for order_id
+        region_id, type_id, order_id, MAX(date) as date, SUM(volume) as volume -- Get most recent order for order_id
     FROM orders
     WHERE date >= date('now','-20 day') -- Number of days to average
     GROUP BY region_id, type_id, order_id -- Include order_id in order to average
-) agg
-GROUP BY agg.region_id, agg.type_id
+) sum_agg
+GROUP BY sum_agg.region_id, sum_agg.type_id, sum_agg.date
+
+-- Step 3: Collapse orders to date
+SELECT avg_agg.region_id, avg_agg.type_id, AVG(avg_agg.volume) as volume
+FROM (
+    SELECT sum_agg.region_id, sum_agg.type_id, sum_agg.date, SUM(sum_agg.volume) as volume
+    FROM (
+        SELECT 
+            region_id, type_id, order_id, MAX(date) as date, SUM(volume) as volume -- Get most recent order for order_id
+        FROM orders
+        WHERE date >= date('now','-20 day') -- Number of days to average
+        GROUP BY region_id, type_id, order_id -- Include order_id in order to average
+    ) sum_agg
+    GROUP BY sum_agg.region_id, sum_agg.type_id, sum_agg.date
+) avg_agg 
+GROUP BY avg_agg.region_id, avg_agg.type_id
